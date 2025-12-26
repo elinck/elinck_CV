bb6e98a28cf785c4a392e4256a5855509f435078
Linck <ethanblinck@gmail.com>
2025-12-26 14:11:04 -0700

    switch to overleaf

--- command.sh
+++
@@ -1,1 +0,0 @@
-pandoc -o elinck_CV.pdf elinck_CV.md -V geometry:margin=1in -V fontsize:10.5pt --pdf-engine=xelatex
--- command_gov.sh
+++
@@ -1,1 +0,0 @@
-pandoc -o elinck_gov_CV.pdf elinck_gov_CV.md -V geometry:margin=1in -V fontsize:10.5pt --pdf-engine=xelatex
